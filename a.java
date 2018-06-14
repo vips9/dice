299c12bdf1041012b08e65c3c40d5706abf54a1e
Vipul Srivastava <vipul.srivastava9@gmail.com>
2018-06-14 17:07:16 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,20 +1,22 @@
-// import java.util.Random;
+
 import java.util.Scanner;
 public class dice{
 
 
 int size;
- int roll(int size)
+int roll(int size)
  {
-	System.out.println(Math.random()%size);
+	//return((int)Math.random()%size);
+		return(((int)(Math.random()*100)%size));
 	
  }
  public static void main(String[] args)
  {
+     String s[]={"one","two","three","four","five","six"};
 	 dice o=new dice();
  Scanner sc=new Scanner(System.in);
 System.out.println("Enter the size :");
 o.size=sc.nextInt();
-o.roll(o.size);
+System.out.println(s[(o.roll(o.size))]);
  }
 }
